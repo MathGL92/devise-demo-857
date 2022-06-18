e804652ce857cb77dbcf35100813b4b0ecd4ec4c
Mathieu Longe <mathieu.longe@orange.fr>
2022-06-18 11:23:07 +1000

    add reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_18_004441) do
+ActiveRecord::Schema.define(version: 2022_06_18_012232) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
