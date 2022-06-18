352fa528d02cb2937d67c2398eb6a220808e7017
Mathieu Longe <mathieu.longe@orange.fr>
2022-06-18 11:21:14 +1000

    add flat table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_18_004441) do
+ActiveRecord::Schema.define(version: 2022_06_18_012050) do
+
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
